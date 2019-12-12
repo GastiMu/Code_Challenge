9b9b6d399bc364bb5ddc2246444e4863a52fc2b1
Gaston <gaston_muzas@hotmail.com>
2019-12-12 03:01:00 -0300

    Terminada parte obligatoria

--- code_challenge/VerificadorCodigos.java
+++ code_challenge/VerificadorCodigos.java
@@ -11,10 +11,25 @@ package code_challenge;
     
     /*Main para ejecucion de pruebas*/
     public static void main (String [] args){
-        String productos[] = new String [10];
-        cargaTest(productos);
-        ordenarAlfabeticamente(productos);
-        imprimirLista(productos);
+        String productos1[] = new String [5];
+        String productos2[] = new String [5];
+        String productos3[];
+        
+        cargaTest(productos1);
+        cargaTest1(productos2);
+       /* 
+        ordenarAlfabeticamente(productos1);
+        imprimirLista(productos2);
+        
+        ordenarAlfabeticamente(productos2);
+        imprimirLista(productos2);
+        
+        productos3 = unirListas(productos1, productos2);
+        imprimirLista(productos3);
+        
+        productos3 = intersectarListas(productos1, productos2);
+        imprimirLista(productos3);
+        */
         
     }
     
@@ -22,15 +37,18 @@ package code_challenge;
     con formato XXX-NNNNN-Y, donde  X: letra(A-Z), N: numero(0-9), Y: numero(0-9)*/
     public static void cargaTest(String productos[]){
         productos[0] = "ABC-12345-8";
-        productos[1] = "BAC-88578-9";
-        productos[2] = "BCD-88578-9";
-        productos[3] = "ABD-88578-9";
-        productos[4] = "BAD-88578-9";  
-        productos[5] = "AAZ-12345-8";
-        productos[6] = "ABB-88578-9";
-        productos[7] = "BBB-88578-9";
-        productos[8] = "BBD-88578-9";
-        productos[9] = "AWD-88578-9";  
+        productos[1] = "BCA-34389-10";
+        productos[2] = "ZZY-85432-6";
+        productos[3] = "RRM-33333-3";
+        productos[4] = "ZZY-85432-6";
+    }
+    
+    public static void cargaTest1(String productos[]){
+        productos[0] = "BCA-34379-10";
+        productos[1] = "BCA-34389-10";
+        productos[2] = "ZZY-85432-7";
+        productos[3] = "RRM-33333-3";
+        productos[4] = "BAC-34389-10";
     }
     
     
@@ -136,14 +154,156 @@ package code_challenge;
             ordenarAlfabeticamenteAux(productos, derecha+1, fin);
     }
       
+    /*Modulo que imprime una lista de productos por pantalla*/
     public static void imprimirLista(String [] lista){
         for(int i = 0; i < lista.length; i++)
             System.out.println(lista[i]);
     }
     
-/*Realizar una función que recibe 2 listas de productos (listas/arrays de los códigos) y
-devuelve una lista/array que representa la Unión de los elementos.*/
+    /*Modulo que recibe 2 listas de productos y devuelve la union de ambas. Si un producto esta en 
+    ambas listas, en la resultante solo estara una vez*/
+    public static String[] unirListas(String [] lista1, String [] lista2){
+        
+        boolean repetido;
+        int index = 0, index3 = 0;
+        String [] lista3 = new String [lista1.length + lista2.length];
+        
+        /*while(index < lista1.length){
+            repetido = verificarExistencia(lista1[index], lista3, index3);
+            if(!repetido){
+                lista3[index3] = lista1[index];
+                index++;
+                index3++;
+            }
+            else {
+                index++;
+            }
+        }*/
+        index3 = unirListasAux(lista1, index, lista3, index3);
+        index = 0;
+        
+        /*while(index < lista2.length){
+            repetido = verificarExistencia(lista2[index], lista3, index3);
+            if(!repetido){
+                lista3[index3] = lista2[index];
+                index++;
+                index3++;
+            }
+            else {
+                index++;
+            }
+        }*/
+        index3 = unirListasAux(lista2, index, lista3, index3);
+        
+        lista3 = optimizarMemoria(lista3, index3);
+        
+        return lista3;
+    }
+    
+    /*Modulo que sirve de auxiliar para verificar y asignar en caso de union, para evitar repetir codigo*/
+    private static int unirListasAux(String [] lista, int indiceLista, String [] resultado, int indiceResultado){
+        boolean repetido;
+        
+        while(indiceLista < lista.length){
+            repetido = verificarExistencia(lista[indiceLista], resultado, indiceResultado);
+                if(!repetido){
+                    resultado[indiceResultado] = lista[indiceLista];
+                    indiceLista++;
+                    indiceResultado++;
+                }
+                else {
+                    indiceLista++;
+                }
+        }
+        return indiceResultado;
+    }
+    
+    /*Modulo auxiliar que verifica si un producto ya pertenece a la union de listas*/
+    private static boolean verificarExistencia(String codigo, String [] lista, int maximoCargado){
+        boolean existe = false;
+        int i = 0;
+        
+        while(i < maximoCargado && !existe){
+            existe = codigo.equalsIgnoreCase(lista[i]);
+            i++;
+        }
+        
+        return existe;
+    }
+    
+    /*Modulo que optimiza la memoria en caso que el arreglo resultante ocupe mas memoria de la 
+    requerida por la union (por ejemplo, si son dos listas grandes con muchos productos en comun)*/
+    private static String[] optimizarMemoria(String [] lista, int index){
+        String [] nuevaLista = new String [index];
+        
+        System.arraycopy(lista, 0, nuevaLista, 0, index);
+        
+        return nuevaLista;
+    }
 
-/*Realizar una función que recibe 2 listas de productos (listas/arrays de los códigos) y
-devuelve una lista/array que representa la Intersección de los elementos*/
+    /*Modulo que recibe 2 listas de productos y devuelve la interseccion de ambas, la resultante siempre
+    toma el menor tamaño entre ambas*/
+    public static String[] intersectarListas(String [] lista1, String [] lista2){
+        String [] lista3;
+        boolean repetido;
+        int index = 0, index3 = 0;
+        
+        if(lista1.length > lista2.length){
+            lista3 = new String [lista2.length];
+            /*
+            while(index < lista2.length){
+                repetido = verificarExistencia(lista2[index], lista1, lista1.length);
+                if(repetido){
+                    lista3[index3] = lista2[index];
+                    index++;
+                    index3++;
+                }
+                else {
+                    index++;
+                }
+            }*/
+            index3 = intersectarListasAux(lista1, lista2, index, lista3, index3);
+        }
+        else{
+            lista3 = new String [lista1.length];
+            /*
+            while(index < lista1.length){
+                repetido = verificarExistencia(lista1[index], lista2, lista2.length);
+                if(repetido){
+                    lista3[index3] = lista1[index];
+                    index++;
+                    index3++;
+                }
+                else {
+                    index++;
+                }
+            }*/
+            index3 = intersectarListasAux(lista2, lista1, index, lista3, index3);
+        }
+            
+        
+        lista3 = optimizarMemoria(lista3, index3);
+        
+        return lista3;
+    }
+    
+    /*Modulo que sirve de auxiliar para verificar y asignar en caso de interseccion, 
+    para evitar repetir codigo*/
+    private static int intersectarListasAux(String [] lista1, String [] lista2, int indiceLista, String [] resultado, int indiceResultado){
+        boolean repetido;
+        
+        while(indiceLista < lista1.length){
+            repetido = verificarExistencia(lista1[indiceLista], lista2, lista2.length);
+            if(repetido){
+                resultado[indiceResultado] = lista1[indiceLista];
+                indiceLista++;
+                indiceResultado++;
+            }
+            else {
+                indiceLista++;
+            }
+        }
+        
+        return indiceResultado;
+    }
 }
